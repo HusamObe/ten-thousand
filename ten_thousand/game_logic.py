68d544921c257266f869880ef237e07a5ae8e4f9
Husam Obeidat <husamobe1991@gmail.com>
2023-05-21 06:47:01 +0000

    finished the validate test

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -48,61 +48,26 @@ class GameLogic:
 
         return score
 
-    def play(self):
-        print("Welcome to Ten Thousand")
-        choice = input("(y)es to play or (n)o to decline\n> ")
+    @staticmethod
+    def validate_keepers(roll, keepers):
+        if len(keepers) > len(roll):
+            return False
 
-        if choice.lower() != "y":
-            print("OK. Maybe another time")
-            return
+        roll_counts = [0] * 7
+        keepers_counts = [0] * 7
 
-        total_score = 0
-        round_num = 1
+        for die in roll:
+            roll_counts[die] += 1
 
-        while True:
-            print(f"Starting round {round_num}")
-            round_score = 0
-            num_dice = 6
+        for die in keepers:
+            keepers_counts[die] += 1
 
-            while True:
-                print(f"Rolling {num_dice} dice...")
-                dice = self.roll_dice(num_dice)
-                print("***", " ".join(str(d) for d in dice), "***")
+        for i in range(1, 7):
+            if keepers_counts[i] > roll_counts[i]:
+                return False
 
-                keep = input("Enter dice to keep, or (q)uit:\n> ")
-
-                if keep.lower() == "q":
-                    print(f"Thanks for playing. You earned {total_score} points")
-                    return
-
-                try:
-                    keep_values = [int(value) for value in keep.split()]
-                    keep_dice = tuple(die for die in dice if die in keep_values)
-
-                    round_score += self.calculate_score(keep_dice)
-                    num_dice -= len(keep_dice)
-
-                    if num_dice != 0:
-                        print(
-                            f"You have {round_score} unbanked points and 0 dice remaining"
-                        )
-
-                        if round_score == 0:
-                            print("No points scored in this round. Round ends.")
-                        else:
-                            total_score += round_score
-                            print(
-                                f"You banked {round_score} points in round {round_num}"
-                            )
-                            print(f"Total score is {total_score} points")
-
-                        round_num += 1
-                        break
-
-                except ValueError:
-                    print("Invalid input. Please try again.")
-
+        return True
 
 if __name__ == "__main__":
     gameR = GameLogic()
-    gameR.play()
+    
